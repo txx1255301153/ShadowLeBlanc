a83ecc72eb71e5377062e00244256e0c08f8cfa7
TheCallunxz <callunxz@hotmail.co.uk>
2018-06-03 09:25:27 +0100

    First Commit

---
+++ Shadow LeBlanc.lua
@@ -0,0 +1,7 @@
+--***Shadow LeBlanc***--
+
+if myHero.charName ~= "LeBlanc" then return end
+
+local version,author,lVersion = "v1.0","TheCallunxz","8.11"
+
+PrintChat("Thank you for using Shadow LeBlanc | " ..version.. "")
